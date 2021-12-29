b52781e1687e143659a8bf743774ab202a80c9b1
Albert <akuanakrajin7@gmail.com>
2021-12-30 05:48:55 +0700

    Tugas 7 Java Script

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggi_badan = [152, 265, 170, 166, 157, 179, 168];
+var n_data = tinggi_badan.length;
+for (var a = 0; a < n_data ; a++){
+  console.log(tinggi_badan[a]);
+}
+console.log("============");
+for(let x of tinggi_badan){
+  console.log(x);
+}
+
+// Sama saja hasilnya dilihat dari hasil tampilan data
